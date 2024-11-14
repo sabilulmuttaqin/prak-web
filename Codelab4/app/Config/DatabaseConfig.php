8de7fac652d3852e4a111b5d89df5189a6e2a643
sabilulmuttaqin <126049379+sabilulmuttaqin@users.noreply.github.com>
2024-11-14 17:34:41 +0700

    Add files via upload

---
+++ Codelab4/app/Config/DatabaseConfig.php
@@ -0,0 +1,11 @@
+<?php 
+namespace app\Config;
+
+class DatabaseConfig
+{
+    public $host = "localhost";
+    public $username = "root";
+    public $password = "";
+    public $database_name = "products";
+    public $port = "3306";
+}
